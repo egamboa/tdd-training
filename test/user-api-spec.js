65c8927b0169361cd625ad26a16172af11946802
Andres Gamboa <estebangam1@gmail.com>
2017-06-22 13:00:00 -0600

    step6 - DataLayer

--- test/user-api-spec.js
+++ test/user-api-spec.js
@@ -1,6 +1,12 @@
 require('co-mocha');
 var should = require('should');
 var data = require('../user.data');
+var fs = require('co-fs');
+
+before(function *() {
+  yield fs.writeFile('./users.json', '[]');
+});
+
 describe('user data', function () {
   it('should have +1 user count after saving', function *() {
     var users = yield data.users.get();
